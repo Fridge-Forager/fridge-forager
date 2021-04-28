c9c72c5d21841189e7f19275f155cbdc3ae6638e
emagdaeh <adam.whitmanpt@gmail.com>
2021-04-28 12:51:18 -0700

    Working test "hi"

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  // templateUrl: './sidebar.component.html',
+  template: `<div>Hi</div>`,
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
