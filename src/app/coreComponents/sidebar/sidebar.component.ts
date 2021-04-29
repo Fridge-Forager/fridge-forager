c05aef09ae8b104ba5153ea17dc38bede5a6eb04
dannyhannyford <dannyhannyford@gmail.com>
2021-04-28 20:20:15 -0700

    dashboard

--- src/app/coreComponents/sidebar/sidebar.component.ts
+++ src/app/coreComponents/sidebar/sidebar.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
-  // templateUrl: './sidebar.component.html',
-  template: `<div>Hi</div>`,
+  templateUrl: './sidebar.component.html',
+  // template: `<div>Hi</div>`,
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
