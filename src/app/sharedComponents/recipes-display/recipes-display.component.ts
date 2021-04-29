bdaf9f944d06fc2469a4ab8591e4e285d5e0d6db
jzhengcode <jzhengcode@gmail.com>
2021-04-29 12:21:37 -0700

    initial setup for recipe card

--- src/app/sharedComponents/recipes-display/recipes-display.component.ts
+++ src/app/sharedComponents/recipes-display/recipes-display.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+export class RecipeCard {
+  title!: string;
+  url!: string;
+  image!: string;
+}
+
 @Component({
   selector: 'app-recipes-display',
   templateUrl: './recipes-display.component.html',
   styleUrls: ['./recipes-display.component.scss']
 })
 export class RecipesDisplayComponent implements OnInit {
-
+  recipe: RecipeCard = {
+    title: "1. Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs",
+    url: "http://fullbellysisters.blogspot.com/2012/06/pasta-with-garlic-scallions-cauliflower.html",
+    image: "https://spoonacular.com/recipeImages/716429-556x370.jpg"
+  }
   constructor() { }
 
   ngOnInit(): void {
