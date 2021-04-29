960c60a98142321c959c09e0c910c3ad29e2737e
emagdaeh <adam.whitmanpt@gmail.com>
2021-04-28 20:11:57 -0700

    Stuck on sidebar component

--- src/app/coreComponents/sidebar/sidebar.component.ts
+++ src/app/coreComponents/sidebar/sidebar.component.ts
@@ -2,11 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
-  // templateUrl: './sidebar.component.html',
-  template: `<div>Hi</div>`,
+  templateUrl: './sidebar.component.html',
+  // template: `<div>Hi</div>`,
   styleUrls: ['./sidebar.component.scss']
 })
+
 export class SidebarComponent implements OnInit {
+  ingredient: string;
+  ingredients = [];
+
+  onClick() {
+    this.ingredients.push({name: this.ingredient});
+    this.ingredient = '';
+  }
 
   constructor() { }
 
