4d7590661d3bed73e8b27ea5067d804178307c93
emagdaeh <adam.whitmanpt@gmail.com>
2021-04-29 10:54:32 -0700

    Completed sidebar functionality without linking to recipes

--- src/app/coreComponents/sidebar/sidebar.component.ts
+++ src/app/coreComponents/sidebar/sidebar.component.ts
@@ -1,24 +1,33 @@
+import { Ingredient } from './models/Ingredients';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  // template: `<div>Hi</div>`,
   styleUrls: ['./sidebar.component.scss']
 })
 
 export class SidebarComponent implements OnInit {
-  ingredient: string;
-  ingredients = [];
+  title = 'Add Ingredients: ';
+  ingredients!: Ingredient[];
+  inputIngredient: string = '';
 
-  onClick() {
-    this.ingredients.push({name: this.ingredient});
-    this.ingredient = '';
+  addItem() {
+    this.ingredients.push({
+      content: this.inputIngredient
+    })
+
+    this.inputIngredient = '';
+  }
+
+  removeItem(id: number) {
+    this.ingredients = this.ingredients.filter((val, i) => i !== id);
   }
 
   constructor() { }
 
   ngOnInit(): void {
+    this.ingredients = []
   }
 
 }
