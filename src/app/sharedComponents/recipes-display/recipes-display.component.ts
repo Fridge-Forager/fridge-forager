b094fe301f3e5e3ad631e307edbb53bb6f88e0bf
jzhengcode <jzhengcode@gmail.com>
2021-04-29 12:31:02 -0700

    display will load an array of recipes

--- src/app/sharedComponents/recipes-display/recipes-display.component.ts
+++ src/app/sharedComponents/recipes-display/recipes-display.component.ts
@@ -6,17 +6,41 @@ export class RecipeCard {
   image!: string;
 }
 
+const RECIPECARDS: RecipeCard[] = [
+  {
+    title: "1. Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs",
+    url: "http://fullbellysisters.blogspot.com/2012/06/pasta-with-garlic-scallions-cauliflower.html",
+    image: "https://spoonacular.com/recipeImages/716429-240x150.jpg"
+  },
+  {
+    title: "2. Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs",
+    url: "http://fullbellysisters.blogspot.com/2012/06/pasta-with-garlic-scallions-cauliflower.html",
+    image: "https://spoonacular.com/recipeImages/716424-240x150.jpg"
+  },
+  {
+    title: "3. Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs",
+    url: "http://fullbellysisters.blogspot.com/2012/06/pasta-with-garlic-scallions-cauliflower.html",
+    image: "https://spoonacular.com/recipeImages/716427-240x150.jpg"
+  },
+  {
+    title: "4. Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs",
+    url: "http://fullbellysisters.blogspot.com/2012/06/pasta-with-garlic-scallions-cauliflower.html",
+    image: "https://spoonacular.com/recipeImages/716426-240x150.jpg"
+  },
+  {
+    title: "5. Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs",
+    url: "http://fullbellysisters.blogspot.com/2012/06/pasta-with-garlic-scallions-cauliflower.html",
+    image: "https://spoonacular.com/recipeImages/716425-240x150.jpg"
+  }
+]
+
 @Component({
   selector: 'app-recipes-display',
   templateUrl: './recipes-display.component.html',
   styleUrls: ['./recipes-display.component.scss']
 })
 export class RecipesDisplayComponent implements OnInit {
-  recipe: RecipeCard = {
-    title: "1. Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs",
-    url: "http://fullbellysisters.blogspot.com/2012/06/pasta-with-garlic-scallions-cauliflower.html",
-    image: "https://spoonacular.com/recipeImages/716429-556x370.jpg"
-  }
+  recipes= RECIPECARDS;
   constructor() { }
 
   ngOnInit(): void {
